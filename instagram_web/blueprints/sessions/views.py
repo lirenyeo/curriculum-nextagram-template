4702f6c3760b1d9331c55ed276c3ce2232dedba4
Liren <liren90@gmail.com>
2019-09-04 15:13:45 +0800

    google oauth setup

--- instagram_web/blueprints/sessions/views.py
+++ instagram_web/blueprints/sessions/views.py
@@ -2,18 +2,19 @@ from flask_login import login_user, logout_user
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from app import login_manager
 from models.user import User
+from instagram_web.util.google_oauth import oauth
 
-sesssions_blueprint = Blueprint('sessions',
+sessions_blueprint = Blueprint('sessions',
                                 __name__,
                                 template_folder='templates')
 
 
-@sesssions_blueprint.route('/new', methods=['GET'])
+@sessions_blueprint.route('/new', methods=['GET'])
 def new():
     return render_template('sessions/new.html')
 
 
-@sesssions_blueprint.route('/', methods=['POST'])
+@sessions_blueprint.route('/', methods=['POST'])
 def create():
     user = User.get_or_none(User.username == request.form.get('username'))
 
@@ -26,7 +27,23 @@ def create():
     return render_template('sessions/new.html')
 
 
-@sesssions_blueprint.route('/delete')
+@sessions_blueprint.route('/delete')
 def destroy():
     logout_user()
     return redirect(url_for('sessions.new'))
+
+
+@sessions_blueprint.route('/authorize/google')
+def authorize():
+    breakpoint()
+    oauth.google.authorize_access_token()
+    email = oauth.google.get('https://www.googleapis.com/oauth2/v2/userinfo').json()['email']
+    user = User.get_or_none(User.email == email)
+
+    if user:
+        login_user(user)
+        flash('You were successfully signed in')
+        return redirect(url_for('users.show', username=user.username))
+    else:
+        flash('Please try signing in/up again or contact support')
+        return redirect(url_for('users.new'))
--- instagram_web/util/s3.py
+++ instagram_web/util/s3.py
@@ -1,19 +1,27 @@
+import os
+import config
 import boto3, botocore
-from config import Config
 from flask import flash
 from flask_login import current_user
 
+
+if os.getenv('FLASK_ENV') == 'production':
+    config = eval("config.ProductionConfig")
+else:
+    config = eval("config.DevelopmentConfig")
+
+
 s3 = boto3.client(
    "s3",
-   aws_access_key_id=Config.S3_KEY,
-   aws_secret_access_key=Config.S3_SECRET
+   aws_access_key_id=config.S3_KEY,
+   aws_secret_access_key=config.S3_SECRET
 )
 
 def upload_file_to_s3(file, acl="public-read"):
     try:
         s3.upload_fileobj(
             file,
-            Config.S3_BUCKET,
+            config.S3_BUCKET,
             f"{current_user.username}/{file.filename}",
             ExtraArgs={
                 "ACL": acl,
